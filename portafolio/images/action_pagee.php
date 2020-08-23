12094c51ab93d902f712c928d46e02e30be1b48f
sebastianquintana <sebastianquintan@gmail.com>
2020-08-23 12:19:04 -0700

    itc-289 project upload to github

---
+++ portafolio/images/action_pagee.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<title>Sebastian Quintana Portfolios</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../portafolio/style.css">
+
+<body class="w3-black">
+<div class=php>
+    <h1><b>Thank you for getting in touch <?php echo $_GET["name"]; ?>! </b></h1>
+    I appreciate you contacting me and I will get back to you at this email address soon!<br>
+ <?php echo $_GET["email"]; ?><br>
+ Have a great day!
+</div>
+</body>
+</html>
+ 
+
